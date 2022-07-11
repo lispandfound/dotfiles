d845aa1acc794066761d9ccd24d99890c0ae61be
Jake Faulkner <jakefaulkn@gmail.com>
2022-07-11 23:16:59 +1200

    let's try out evil again

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -212,16 +212,11 @@ URL and CALLBACK; see `url-queue-retrieve'"
                                (when (string-match "Now serving on \\(.*\\)" line)
                                  (message line))))))
 
+(after! avy
+  (setq avy-keys '(?a ?o ?e ?u ?i ?d ?h ?t ?n ?s)))
+
 
 (add-hook 'after-init-hook #'repeat-mode)
-
-
-
-
-
-
-
-
 
 ;; Whenever you reconfigure a package, make sure to wrap your config in an
 ;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
