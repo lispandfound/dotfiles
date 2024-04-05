6c0f84e38c79c2b2761ef1a52e69650900fdb06c
Jake Faulkner <jake@devbox.phoenix>
2024-04-05 20:58:32 +1300

    Complete refresh of the dot files

--- .emacs.d/tempo.el
+++
@@ -1,9 +0,0 @@
-(tempo-define-template "lambda"
-                          '(> "(lambda (" p ")" n> r> ")">)
-                          nil
-                          "Insert a template for an anonymous procedure")
-
-(define-abbrev emacs-lisp-mode-abbrev-table "lambda" "" 'tempo-template-lambda)
-(tempo-define-template "mm" '("\\(" p "\\)") nil "Template for mathematics in latex")
-(with-eval-after-load 'org
-  (define-abbrev org-mode-abbrev-table "mm" "" 'tempo-template-mm))
