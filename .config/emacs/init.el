df6b68a66f06e404c1d76c8ec96c286d69c64566
Jake Faulkner <jakefaulkn@gmail.com>
2024-06-27 16:46:03 +1200

    transpose frame

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -562,6 +562,15 @@ If the new path's directories does not exist, create them."
 
 (global-set-key (kbd "C-c b") #'clone-buffer-other-window)
 (add-hook 'write-file-hooks 'delete-trailing-whitespace nil t)
+
+
+(use-package transpose-frame
+  :ensure t
+  :bind (("C-x 4 t" . transpose-frame)))
+
+
+
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
