89af11ce0c5aee80687d18b9f7eb07e80a19c1d3
Jake Faulkner <jakefaulkn@gmail.com>
2024-07-05 09:06:59 +1200

    disable package.el

---
+++ .config/emacs/early-init.el
@@ -0,0 +1,11 @@
+
+;; Startup speed, annoyance suppression
+(setq gc-cons-threshold 10000000)
+(setq byte-compile-warnings '(not obsolete))
+(setq warning-suppress-log-types '((comp) (bytecomp)))
+(setq native-comp-async-report-warnings-errors 'silent)
+
+;; Silence stupid startup message
+(setq inhibit-startup-echo-area-message (user-login-name))
+
+(setq package-enable-at-startup nil)
