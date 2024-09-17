366821822db5446d31aaf86731ebaa8bbacbf7de
Jake Faulkner <jakefaulkn@gmail.com>
2024-09-17 16:47:22 +1200

    add custom variables

--- .config/emacs/custom.el
+++ .config/emacs/custom.el
@@ -8,31 +8,43 @@
  '(consult-narrow-key "<")
  '(display-line-numbers t)
  '(org-capture-templates
-   '(("n" "Note" entry (file+headline "~/org/todo.org" "Notes")
+   '(("l" "Logged completed task" entry
+      (file+headline "~/org/todo.org" "Tasks")
+      "* DONE %?\12 %U\12 %a\12 %i")
+     ("n" "Note" entry (file+headline "~/org/todo.org" "Notes")
       "* %?\12 %U\12 %a\12 %i")
      ("t" "Todo" entry (file+headline "~/org/todo.org" "Tasks")
       "* TODO %?\12 %U\12 %a\12 %i")))
  '(package-install-upgrade-built-in t)
  '(package-selected-packages
-   '(adaptive-wrap apheleia ascii-art-to-unicode auto-virtualenv
-                   better-defaults browse-at-remote cape casual-avy
-                   casual-calc casual-dired casual-info casual-isearch
-                   casual-suite consult-dir consult-eglot corfu crux
-                   csv-mode devdocs doom-themes dumb-jump
-                   edit-indirect elm-mode embark-consult embrace
+   '(adaptive-wrap apheleia apptainer-mode ascii-art-to-unicode
+                   auto-virtualenv better-defaults browse-at-remote
+                   cape casual-avy casual-calc casual-dired
+                   casual-info casual-isearch casual-suite consult-dir
+                   consult-eglot corfu counsel-pydoc crux csv-mode
+                   devdocs doom-themes dumb-jump edit-indirect
+                   elm-mode embark-consult embrace
                    exec-path-from-shell expreg forge gptel grip-mode
                    haskell-mode htmlize ibuffer-project jinx just-mode
                    ligature marginalia mermaid-mode moody numpydoc
                    orderless org-menu org-reveal ox-reveal
-                   pandoc-transient skempo titlecase transpose-frame
-                   treesit-auto vertico visual-fill-column wgrep
-                   ws-butler yaml-mode zoxide))
+                   pandoc-transient popper pydoc shackle skempo
+                   titlecase transpose-frame treesit-auto vertico
+                   visual-fill-column wgrep ws-butler yaml-mode zoxide))
  '(package-vc-selected-packages
-   '((org-menu :url "https://github.com/sheijk/org-menu")
+   '((apptainer-mode :vc-backend Git :url
+                     "https://github.com/jrgant/apptainer-mode")
+     (org-menu :url "https://github.com/sheijk/org-menu")
      (pandoc-transient :url
                        "https://github.com/lispandfound/pandoc-transient")
      (skempo :url "https://github.com/xFA25E/skempo")))
+ '(popper-reference-buffers
+   '("Output\\*$" "\\*Async Shell Command\\*" compilation-mode help-mode
+     eshell-mode "\\*Messages\\*$" devdocs-mode))
+ '(popper-window-height 20)
  '(register-preview-delay 0.5)
+ '(shackle-mode t)
+ '(shackle-rules '((help-mode :popup t) (eshell-mode :popup t)))
  '(use-package-always-defer t)
  '(use-package-always-ensure t))
 (custom-set-faces
