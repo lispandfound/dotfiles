437cde3d709aa81c6b58ed03ef01ad934ab033dc
Jake Faulkner <jakefaulkn@gmail.com>
2025-01-17 09:20:46 +1300

    add work configuration

--- flake.nix
+++ flake.nix
@@ -43,6 +43,21 @@
 
         }
       ];
+      };
+      nixosConfigurations.work = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ./hosts/work/configuration.nix
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+
+          # TODO replace ryan with your own username
+          home-manager.users.jake = import ./hosts/work/home.nix;
+
+        }
+      ];
     };
   };
 }
---
+++ modules/nvidia.nix
@@ -0,0 +1,4 @@
+{ config, pkgs, ... }: {
+ hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.stable; 
+ services.xserver.videoDrivers = [ "nvidia" ]; 
+}
