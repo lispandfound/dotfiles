471a66d0e1594fcd2f38fcfb25c067a88c11bde4
Jake Faulkner <jakefaulkn@gmail.com>
2025-01-16 15:27:13 +1300

    github cli and virtual env switch removal

--- modules/shell.nix
+++ modules/shell.nix
@@ -29,6 +29,7 @@
     devenv
     direnv
     vault-tasks
+    gh
   ];
   home.file.".config/vault-tasks/config.toml".source =
     ../config/vault-tasks/config.toml;
@@ -108,29 +109,6 @@
     interactiveShellInit = ''
       set -g fish_greeting ""
       fzf_configure_bindings
-      function __auto_source_venv --on-variable PWD --description "Activate/Deactivate virtualenv on directory change"
-        status --is-command-substitution; and return
-
-        # Check if we are inside a git directory
-        if git rev-parse --show-toplevel &>/dev/null
-          set gitdir (realpath (git rev-parse --show-toplevel))
-          set cwd (pwd -P)
-          # While we are still inside the git directory, find the closest
-          # virtualenv starting from the current directory.
-          while string match "$gitdir*" "$cwd" &>/dev/null
-            if test -e "$cwd/.venv/bin/activate.fish"
-              source "$cwd/.venv/bin/activate.fish" &>/dev/null 
-              return
-            else
-              set cwd (path dirname "$cwd")
-            end
-          end
-        end
-        # If virtualenv activated but we are not in a git directory, deactivate.
-        if test -n "$VIRTUAL_ENV"
-          deactivate
-        end
-      end
       function fzf-man
           if test -n "$argv[1]"
               man $argv
