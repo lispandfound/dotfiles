aeb6b8e1de4ef6dcf634528e1135a1ee12220ca9
Jake Faulkner <jakefaulkn@gmail.com>
2025-01-21 09:03:41 +1300

    fix display manager warnings

--- hosts/work/configuration.nix
+++ hosts/work/configuration.nix
@@ -46,8 +46,8 @@
   services.xserver.enable = true;
 
   # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.sddm.enable = true;
-  services.xserver.desktopManager.plasma6.enable = true;
+  services.displayManager.sddm.enable = true;
+  services.desktopManager.plasma6.enable = true;
 
   # Configure keymap in X11
   services.xserver.xkb = {
