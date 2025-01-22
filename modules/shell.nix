cea1647fe3e0c239bc01db5b1c2e572fad8a8d66
Jake Faulkner <jakefaulkn@gmail.com>
2025-01-23 09:14:33 +1300

    Yazi shell integration for fish

--- modules/shell.nix
+++ modules/shell.nix
@@ -126,6 +126,14 @@
               return $status
           end
       end
+      function y
+        	set tmp (mktemp -t "yazi-cwd.XXXXXX")
+        	yazi $argv --cwd-file="$tmp"
+        	if set cwd (command cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+        		builtin cd -- "$cwd"
+        	end
+        	rm -f -- "$tmp"
+      end
     '';
     plugins = [
       {
