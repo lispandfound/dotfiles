4a114eeacac50dd042e58c67c82cedf6d6c70d74
Jake Faulkner <jakefaulkn@gmail.com>
2025-02-20 13:46:01 +1300

    sqlite bundled with emacs

--- modules/emacs.nix
+++ modules/emacs.nix
@@ -1,7 +1,12 @@
 { config, pkgs, ... }:
 
 {
-  home.packages = with pkgs; [ emacs30-pgtk hunspell hunspellDicts.en-au ];
+  home.packages = with pkgs; [
+    emacs30-pgtk
+    hunspell
+    hunspellDicts.en-au
+    sqlite
+  ];
   home.file."${config.home.homeDirectory}/.config/doom" = {
     source = ../config/doom;
     target = "${config.home.homeDirectory}/.config/doom";
