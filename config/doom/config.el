dc31c68df742490998366001c75ee3bb71d26cf9
Jake Faulkner <jakefaulkn@gmail.com>
2025-02-20 13:46:23 +1300

    open dash docsets in firefox

--- config/doom/config.el
+++ config/doom/config.el
@@ -58,6 +58,8 @@ evil-ex-substitute-global
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
 (setq org-directory "~/org/")
+(after! dash-docs
+  (setq dash-docs-browser-func #'browse-url-firefox))
 
 
 ;; Whenever you reconfigure a package, make sure to wrap your config in an
