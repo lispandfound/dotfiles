3e0fb40b489864d38b94506e4a5139044f6a7252
Jake Faulkner <jakefaulkn@gmail.com>
2025-04-14 17:05:10 +1200

    fix themes

--- config/emacs/custom.el
+++ config/emacs/custom.el
@@ -75,6 +75,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :extend nil :stipple nil :background "#282c34" :foreground "#bbc2cf" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight regular :height 120 :width normal :foundry "SRC" :family "JetBrainsMono Nerd Font"))))
- '(mode-line ((t (:background "#1d2026" :foreground "#bbc2cf" :box (:line-width (2 . 6) :style flat-button)))))
- '(mode-line-inactive ((t (:background "#21242b" :foreground "#5B6268" :box (:line-width (2 . 6) :style flat-button))))))
+ '(default ((t (:inherit nil :extend nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight regular :height 120 :width normal :foundry "JB" :family "JetBrainsMono Nerd Font"))))
+ '(mode-line ((t (:box (:line-width (2 . 6) :style flat-button)))))
+ '(mode-line-inactive ((t (:box (:line-width (2 . 6) :style flat-button))))))
