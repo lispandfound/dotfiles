824659fd1c160f93f1a11c7dfaa3bd4b0e39a8b3
Jake Faulkner <jake.faulkner@canterbury.ac.nz>
2026-04-21 09:12:58 +1200

    fix justfile formatting

--- config/doom/modules/lang/just/config.el
+++ config/doom/modules/lang/just/config.el
@@ -1,23 +1,8 @@
 ;;; lang/just/config.el -*- lexical-binding: t; -*-
-(cl-defun just-formatter (&key scratch callback &allow-other-keys)
-  "Format a Justfile using a temporary file because just --fmt refuses stdin."
-  (let ((temp (make-temp-file "justfile-")))
-    (with-current-buffer scratch
-      (write-region (point-min) (point-max) temp nil 'silent))
-    
-    (if (zerop (call-process "just" nil nil nil "--unstable" "--fmt" "-j" temp))
-        (with-current-buffer scratch
-          (erase-buffer)
-          (insert-file-contents temp)
-          (delete-file temp)
-          (funcall callback nil))
-      
-      (delete-file temp)
-      (funcall callback '(error "Just formatting failed")))))
+
 
 (use-package! just-ts-mode
-
   :config
   (set-formatter! 'just-format
-    #'just-formatter
+    '("just" "--fmt" "--unstable" "--justfile" filepath)
     :modes '(just-ts-mode)))
