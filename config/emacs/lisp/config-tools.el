25b5e5311c8212279b5f7213f93c09efc9d9374e
Jake Faulkner <jake.faulkner@canterbury.ac.nz>
2026-07-14 10:42:32 +1200

    add git gutter

--- config/emacs/lisp/config-tools.el
+++ config/emacs/lisp/config-tools.el
@@ -202,6 +202,41 @@ skipped.  Session-level decisions are honoured.  Otherwise prompts:
 
 (use-package magit-lfs :after magit)
 
+
+(use-package git-gutter
+  :hook (prog-mode . git-gutter-mode)
+  :config
+  (setq git-gutter:update-interval 0.02))
+
+(use-package git-gutter-fringe
+  :config
+  (fringe-helper-define 'git-gutter-fr:added '(center repeated)
+    "XXX....."
+    "XXX....."
+    "XXX....."
+    "XXX.....")
+  (fringe-helper-define 'git-gutter-fr:modified '(center repeated)
+    "XXX....."
+    "XXX....."
+    "XXX....."
+    "XXX.....")
+  (fringe-helper-define 'git-gutter-fr:deleted 'bottom
+    "X......."
+    "XX......"
+    "XXX....."
+    "XXXX...."
+    "XXXXX..."
+    "XXXXXX.."
+    "XXXXXXX."
+    "XXXXXXXX"
+    "XXXXXXX."
+    "XXXXXX.."
+    "XXXXX..."
+    "XXXX...."
+    "XXX....."
+    "XX......"
+    "X......."))
+
 ;;; =========================================================================
 ;;; GIT-LINK (C-c v l)
 ;;; =========================================================================
